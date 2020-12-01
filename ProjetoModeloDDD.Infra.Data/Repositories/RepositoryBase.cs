0f0a18154bc24fbdcddbd7951476eb7ea142dbe3
Guilherme Volpe Vitorino da Silva <c110628@corp.caixa.gov.br>
2020-12-01 17:32:42 -0300

    Inicio

---
+++ ProjetoModeloDDD.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ProjetoModeloDDD.Domain.Interfaces;
+
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoty<TEntity> where TEntity : class 
+    {
+    }
+}
